bc5b226b48f9827f4a9676ebd11c954a26f1dfa0
wangdongfang <13838064521@163.com>
2019-06-09 15:21:28 +0800

    test2

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -5,12 +5,13 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        //hellotest
     }
 
     public void test(){
 
         int i = 0;
         i+=10;
-
+        System.out.println(i);
     }
 }
